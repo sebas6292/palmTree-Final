19929d4af7a481de769651b9899e775a45925ace
Sebastian <sebas6292@gmail.com>
2023-06-04 17:28:19 -0700

    clean up and remove green code

--- .history/script_20230604172257.js
+++ .history/script_20230604172257.js
@@ -13,5 +13,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 6000); 
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
